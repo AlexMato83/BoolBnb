b274e44c89f5cc84594ad71e54348c1cb3e0b6bb
AlexMato <matontialessandro0@gmail.com>
2020-07-02 14:37:51 +0200

    model completed

---
+++ boolbnb/app/Apartment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Apartment extends Model
+{
+    protected $table = 'apartments';
+
+    public function owner(){
+
+      return $this->belongsTo(Owner::class);
+    }
+
+    public function category(){
+
+      return $this->belongsTo(Category::class);
+    }
+
+    public function services(){
+
+      return $this->belongsToMany(Service::class);
+    }
+
+    public function messages(){
+
+      return $this->hasMany(Message::class);
+    }
+
+    public function sponsorships(){
+
+      return $this->hasMany(Sponsorship::class);
+    }
+
+
+
+}
