08afd4959e4fd97e57481eaff3b5eb3cd18ead9a
Nunzia <nunzia.fina@gmail.com>
2020-07-11 12:18:28 +0200

    User

--- boolbnb/resources/views/user_apartments.blade.php
+++ boolbnb/resources/views/user_apartments.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.mainLayout')
 
 @section('content')
-  <div class="crea_appartamento">
-    <h1><a href="{{route('create_apartment')}}">Crea nuovo appartamento</a>
-    </h1>
+  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+    <div class="crea_appartamento">
+      <h1><a href="{{route('create_apartment')}}">Crea nuovo appartamento</a>
+      </h1>
+    </div>
+
+    <div class="risultati_appartamenti_proprietario">
+      @foreach ($apartments as $apartment)
+        <div class="appartamenti_proprietario">
+          <div class="appartamenti_proprietario_foto">
+            <a href="{{route('show_upra_apartment',$apartment['id'])}}">
+              <img src="{{ asset($apartment['images'])}}">
+            </a>
+          </div>
+          <div class="appartamenti_proprietario_nome">
+            <h2>
+              <a href="{{route('show_upra_apartment',$apartment['id'])}}">
+                {{$apartment["name"]}}
+              </a>
+            </h2>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </div>
-  <div class="appartamenti_proprietario">
-    @foreach ($apartments as $apartment)
-      <div class="appartamenti_proprietario_foto">
-        <a href="{{route('show_upra_apartment',$apartment['id'])}}">
-          <img src="{{ asset($apartment['images'])}}">
-        </a>
-      </div>
-      <div class="appartamenti_proprietario_nome">
-        <a href="{{route('show_upra_apartment',$apartment['id'])}}">
-          {{$apartment["name"]}}
-        </a>
-      </div>
-
-
-
-    @endforeach
-  </div>
-
-
-
 @endsection
